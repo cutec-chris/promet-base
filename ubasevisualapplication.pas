9d1eeb29ccb6e7936fe5e00c70bf6f60ebab3c3e
Chris <ulrich@tcs-germany.de>
2016-02-08 14:36:23 +0100

    BaseVisualApplication: restore Form Bounds only in not maximized State

--- ubasevisualapplication.pas
+++ ubasevisualapplication.pas
@@ -470,10 +470,14 @@ begin
       if Assigned(MainForm) then
         begin
           aWS := TWindowState(Config.ReadInteger('MainFormState',Integer(wsNormal)));
-          Config.ReadRect('MainFormPos',NewRect,MainForm.BoundsRect);
-          MainForm.BoundsRect := NewRect;
-          if aWs <> wsMinimized then
-            Mainform.WindowState:=aWS;
+          if (aWs <> wsMinimized)
+          and (aWs <> Mainform.WindowState)
+          then
+            begin
+              Config.ReadRect('MainFormPos',NewRect,MainForm.BoundsRect);
+              MainForm.BoundsRect := NewRect;
+              Mainform.WindowState:=aWS;
+            end;
         end;
       except
         DeleteFile(Properties.FileName);
