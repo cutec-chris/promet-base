849ffdbf583bf75723acd4cdeca208873bd32a5c
Chris <ulrich@tcs-germany.de>
2016-03-08 16:02:58 +0100

    DBIntfs:remove network Unit

--- dbintfs/pprometdbintfs.pas
+++ dbintfs/pprometdbintfs.pas
@@ -13,8 +13,7 @@ uses
   ubaseapplicationtools, usync, uSessionDBClasses, utask, uvtools, uimpvcal, 
   uDocumentProcess, uProcessManager, uProcessManagement, uprometipc, umeeting, 
   uBaseDocPages, uthumbnails, ucalc, uimport, uPasswordSave, uzugferd, 
-  uprometscripts, uMeasurement, uwlxprototypes, uWlxModule, uspreadsheet, 
-  uprometmsgnetwork;
+  uprometscripts, uMeasurement, uwlxprototypes, uWlxModule, uspreadsheet;
 
 implementation
 
