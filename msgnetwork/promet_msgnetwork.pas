70b18f7b8c0405f99ed6c2c2d1e151f1fbae0529
Christian Ulrich <info@cu-tec.de>
2016-05-15 19:08:07 +0200

    Runtuime only packages

--- msgnetwork/promet_msgnetwork.pas
+++ msgnetwork/promet_msgnetwork.pas
@@ -2,21 +2,14 @@
   This source is only used to compile and install the package.
  }
 
-unit promet_msgnetwork;
+unit promet_msgnetwork; 
 
-{$warn 5023 off : no warning about unused units}
 interface
 
 uses
-  uprometmsgnetwork, uprometdataserver, uAppServer, uappserverhttp, 
-  uappserverpubsub, uprometwikiserver, LazarusPackageIntf;
+    uprometmsgnetwork, uprometdataserver, uAppServer, uappserverhttp, 
+  uappserverpubsub, uprometwikiserver;
 
 implementation
 
-procedure Register;
-begin
-end;
-
-initialization
-  RegisterPackage('promet_msgnetwork', @Register);
 end.
