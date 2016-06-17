e0cfac56829bc55e195e01a7ee092757e0cb405e
Chris <ulrich@tcs-germany.de>
2016-06-17 15:01:28 +0200

    MsgClient:additional functionality

--- msgclient/uprometmsgclient.pas
+++ msgclient/uprometmsgclient.pas
@@ -8,18 +8,25 @@ uses
   Classes, SysUtils, blcksock, Utils;
 
 type
+  TPublishFunction = procedure(Topic,Value : string);
 
   { TPrometMsgClient }
 
   TPrometMsgClient = class(TThread)
   private
+    FPub: TPublishFunction;
     Sock : TTCPBlockSocket;
     FName : string;
   public
     constructor Create;
     destructor Destroy; override;
     procedure Execute; override;
+    function Connected : Boolean;
     procedure Log(AppName, aType: string; aMsg: string);
+    function Pub(Topic,Value : string) : Boolean;
+    function Sub(Topic : string) : Boolean;
+    function Receive(Timeout : Integer) : string;
+    property OnPublish : TPublishFunction read FPub write FPub;
   end;
 
 implementation
@@ -30,7 +37,7 @@ constructor TPrometMsgClient.Create;
 begin
   Sock := TTCPBlockSocket.Create;
   Sock.ConnectionTimeout:=200;
-  Sock.Connect('localhost','8087');
+  Sock.Connect('127.0.0.1','8087');
   FName := GetSystemName; //Sock.ResolveName(Sock.LocalName);
   inherited Create(False);
 end;
@@ -47,14 +54,36 @@ begin
   while not Terminated do
     begin
       if Sock.LastError<>0 then
-        Sock.Connect('localhost','8087');
+        Sock.Connect('127.0.0.1','8087');
       sleep(500);
     end;
 end;
 
+function TPrometMsgClient.Connected: Boolean;
+begin
+  Result := Sock.LastError=0;
+end;
+
 procedure TPrometMsgClient.Log(AppName,aType: string; aMsg: string);
 begin
-  Sock.SendString('PUB /'+FName+'/'+AppName+'/'+aType+' '+aMsg+CRLF);
+  Pub('/'+FName+'/'+AppName+'/'+aType,aMsg);
+end;
+
+function TPrometMsgClient.Pub(Topic, Value: string): Boolean;
+begin
+  Sock.SendString('PUB '+Topic+' '+Value+CRLF);
+  Result := Receive(100)='OK';
+end;
+
+function TPrometMsgClient.Sub(Topic: string): Boolean;
+begin
+  Sock.SendString('SUB '+Topic+CRLF);
+  Result := True;
+end;
+
+function TPrometMsgClient.Receive(Timeout: Integer): string;
+begin
+  Result := Sock.RecvTerminated(Timeout,CRLF);
 end;
 
 end.
