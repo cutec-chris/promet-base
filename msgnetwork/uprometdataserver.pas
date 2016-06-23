48450e8ca2f1a802ebf3b02e72474ab1d254a80b
Chris <ulrich@tcs-germany.de>
2016-06-23 14:30:35 +0200

    Dataserver:started to implement basic list support

--- msgnetwork/uprometdataserver.pas
+++ msgnetwork/uprometdataserver.pas
@@ -32,29 +32,91 @@ procedure ObjectToJSON(AObject : TBaseDBDataSet; AJSON: TJSONObject;const ADateA
 
 implementation
 
-uses usync,uBaseDatasetInterfaces;
+uses usync,uBaseDatasetInterfaces,uData;
 
 function HandleDataRequest(Method, URL: string;Headers : TStringList;Input,Output : TStream): Integer;
+var
+  aClassType: TBaseDBDatasetClass;
+  aClass: TBaseDBDataset;
+  aList: String;
+  aFilter: String;
+  FSQLStream: TStringStream;
+  QueryFields: TStringList;
+  FSQLScanner: TSQLScanner;
+  FSQLParser: TSQLParser;
+  aStmt: TSQLElement;
+  Json: TJSONArray = nil;
+  aSeq: String;
+  slOutput: TStringList;
 begin
   {
-  /api/jsonrpc/pds/objects/contacts
-  /api/jsonrpc/pds/objects/contacts('1091')
-  /api/jsonrpc/pds/objects/contacts('1091')?setstatus=I
-  /api/jsonrpc/pds/objects/contacts('1091')/address
+  /api/pds/objects/contacts
+  /api/pds/objects/contacts('1091')
+  /api/pds/objects/contacts('1091')?setstatus=I
+  /api/pds/objects/contacts('1091')/address
   http://www.odata.org/
   /wiki/folder1/page2
   }
   Result := 500;
-  if copy(lowercase(url),0,17)='/api/jsonrpc/pds/objects/' then
-    begin
-      url := copy(url,18,length(url));
-
-    end
-  else if copy(lowercase(url),0,15)='/api/jsonrpc/pds/lists/' then
+  if (copy(lowercase(url),0,15)='/api/pds/lists/') and (UpperCase(Method)='GET') then
     begin
       url := copy(url,16,length(url));
-
-    end;
+      aList := copy(url,0,pos('/',url)-1);
+      QueryFields := TStringList.Create;
+      slOutput := TStringList.Create;
+      aFilter := QueryFields.Values['filter'];//TODO:Param filter
+      if aList <> '' then
+        begin
+          if aFilter<>'' then
+            aList := aList+' where '+aFilter;
+          FSQLStream := TStringStream.Create('select * from '+aList);
+        end
+      else
+        FSQLStream := TStringStream.Create(QueryFields.Values['ql']);
+      FSQLScanner := TSQLScanner.Create(FSQLStream);
+      FSQLParser := TSQLParser.Create(FSQLScanner);
+      try
+        aStmt := FSQLParser.Parse;
+        if TSQLSelectStatement(aStmt).Tables.Count = 1 then
+          aList := TSQLSimpleTableReference(TSQLSelectStatement(aStmt).Tables[0]).ObjectName.Name;
+        if Data.DataSetFromName(aList,aClassType) then
+          begin
+            aClass := aClassType.Create(nil);
+            Json := TJSONArray.Create;
+            Result:=200;
+            Headers.Clear;
+            Headers.Add('ContentType: text/javascript;charset=utf-8');
+            Headers.Add('Access-Control-Allow-Origin: *');
+            DataSetToJSON(aClass.DataSet,Json,True,TSQLSelectStatement(aStmt).Fields);
+            aClass.Free;
+          end;
+      except
+        on e : ESQLParser do
+          begin
+            if e.Col > 0 then
+              begin
+                Result := 401;
+                slOutput.Text:='['+IntToStr(e.Line)+':'+IntToStr(e.Col)+'] '+e.Message+','+FSQLParser.CurSource;
+                slOutput.SaveToStream(Output);
+              end;
+          end;
+      end;
+      aSeq := QueryFields.Values['sequence'];
+      if aSeq='' then aSeq := '0';
+      if Assigned(Json) then
+        begin
+          slOutput.Text:=Json.AsJSON;
+          slOutput.SaveToStream(Output);
+          Json.Free;
+        end;
+      slOutput.Free;
+      QueryFields.Free;
+    end
+  else if copy(lowercase(url),0,17)='/api/pds/objects/' then
+    begin
+      url := copy(url,18,length(url));
+    end
+  ;
 end;
 
 procedure DataSetToJSON(ADataSet: TDataSet; AJSON: TJSONArray;
