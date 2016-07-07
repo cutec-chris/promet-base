1d1548389912fd3d55a39d95c1ebcad36527ec0d
Chris <ulrich@tcs-germany.de>
2016-07-07 15:27:05 +0200

    Create Production Order:Dialog advanced

--- base_erp/ucreateproductionorder.pas
+++ base_erp/ucreateproductionorder.pas
@@ -1,3 +1,22 @@
+{*******************************************************************************
+  Copyright (C) Christian Ulrich info@cu-tec.de
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or commercial alternative
+  contact us for more information
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+Created 07.07.2016
+*******************************************************************************}
 unit uCreateProductionOrder;
 
 {$mode objfpc}{$H+}
@@ -6,7 +25,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  StdCtrls, ButtonPanel,uOrder,uMasterdata,variants;
+  StdCtrls, ButtonPanel, EditBtn,uOrder,uMasterdata,variants,uIntfStrConsts;
 
 type
 
@@ -14,11 +33,14 @@ type
 
   TfCreateProductionOrder = class(TForm)
     ButtonPanel1: TButtonPanel;
-    eVersion: TComboBox;
-    eMasterdata: TEdit;
+    eMasterdata: TEditButton;
+    cbVersion: TComboBox;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
+    procedure eMasterdataButtonClick(Sender: TObject);
+    procedure eMasterdataExit(Sender: TObject);
+    function SetOrderfromSearch(aLink: string): Boolean;
   private
     { private declarations }
   public
@@ -31,7 +53,80 @@ var
 
 implementation
 
+uses uSearch;
+
+resourcestring
+  strMasterdataWithoutPiecelist                   = 'Der Artikel hat keine Stückliste, wirklich erstellen ?';
+
 { TfCreateProductionOrder }
+
+procedure TfCreateProductionOrder.eMasterdataButtonClick(Sender: TObject);
+begin
+  fSearch.SetLanguage;
+  fSearch.OnOpenItem:=@SetOrderfromSearch;
+  fSearch.AllowSearchTypes(strMasterdata);
+  fSearch.Execute(True,'PRODSE','');
+end;
+
+procedure TfCreateProductionOrder.eMasterdataExit(Sender: TObject);
+var
+  aMasterdata: TMasterdata;
+begin
+  aMasterdata := TMasterdata.Create(nil);
+  aMasterdata.Select(eMasterdata.Text);
+  aMasterdata.Open;
+  if aMasterdata.Count>0 then
+    begin
+      aMasterdata.Select(aMasterdata.Number.AsString);
+      aMasterdata.Open;
+      eMasterdata.Text:=aMasterdata.Number.AsString;
+      cbVersion.Text:=aMasterdata.Version.AsString;
+      cbVersion.Enabled:=aMasterdata.Count>1;
+      cbVersion.Items.Clear;
+      aMasterdata.First;
+      while not aMasterdata.EOF do
+        begin
+          cbVersion.Items.Add(aMasterdata.Version.AsString);
+          aMasterdata.Next;
+        end;
+      if cbVersion.Enabled then
+        begin
+          aMasterdata.Locate('ACTIVE','Y',[]);
+          cbVersion.Text:=aMasterdata.Version.AsString;
+        end;
+    end;
+  aMasterdata.Free;
+end;
+
+function TfCreateProductionOrder.SetOrderfromSearch(aLink: string): Boolean;
+var
+  aMasterdata: TMasterdata;
+begin
+  aMasterdata := TMasterdata.Create(nil);
+  aMasterdata.SelectFromLink(aLink);
+  aMasterdata.Open;
+  if aMasterdata.Count>0 then
+    begin
+      aMasterdata.Select(aMasterdata.Number.AsString);
+      aMasterdata.Open;
+      eMasterdata.Text:=aMasterdata.Number.AsString;
+      cbVersion.Text:=aMasterdata.Version.AsString;
+      cbVersion.Enabled:=aMasterdata.Count>1;
+      cbVersion.Items.Clear;
+      aMasterdata.First;
+      while not aMasterdata.EOF do
+        begin
+          cbVersion.Items.Add(aMasterdata.Version.AsString);
+          aMasterdata.Next;
+        end;
+      if cbVersion.Enabled then
+        begin
+          aMasterdata.Locate('ACTIVE','Y',[]);
+          cbVersion.Text:=aMasterdata.Version.AsString;
+        end;
+    end;
+  aMasterdata.Free;
+end;
 
 function TfCreateProductionOrder.Execute(FOrder: TOrder; aID: string;
   aVersion: Variant): Boolean;
@@ -43,26 +138,36 @@ begin
       Application.CreateForm(TfCreateProductionOrder,fCreateProductionOrder);
       Self := fCreateProductionOrder;
     end;
+  eMasterdata.Text:=aID;
+  eMasterdataExit(nil);
+  if aVersion<>Null then
+    cbVersion.Text:=aVersion;
   Result := ShowModal=mrOK;
   if Result then
     begin
       //Find Article and Create Order
       aMasterdata := TMasterdata.Create(nil);
-      aMasterdata.SelectFromNumber(aID);
+      aMasterdata.SelectFromNumber(eMasterdata.Text);
       aMasterdata.Open;
-      aMasterdata.Locate('VERSION',aVersion,[]);
+      if aVersion<>Null then
+        aMasterdata.Locate('VERSION',cbVersion.Text,[]);
+      aMasterdata.Positions.Open;
+      if aMasterdata.Positions.Count=0 then
+        begin
+          if MessageDlg(strMasterdata,strMasterdataWithoutPiecelist,mtWarning,[mbYes,mbNo],0) <> mrYes then
+            begin
+              Result:=False;
+              exit;
+            end;
+        end;
       if aMasterdata.Count>0 then
         begin
-          FOrder.OrderType.Open;
-          if FOrder.OrderType.Locate('SI_PROD;TYPE',VarArrayOf(['Y',7]),[]) then
-            begin
-              FOrder.Insert;
-              FOrder.Positions.Insert;
-              //FOrder.Status.AsString:=FOrder.OrderType.FieldByName('STATUS').AsString;
-              FOrder.Positions.Assign(aMasterdata);
-              FOrder.Positions.Post;
-              FOrder.Post;
-            end;
+          FOrder.Insert;
+          FOrder.Positions.Insert;
+          //FOrder.Status.AsString:=FOrder.OrderType.FieldByName('STATUS').AsString;
+          FOrder.Positions.Assign(aMasterdata);
+          FOrder.Positions.Post;
+          FOrder.Post;
         end;
       aMasterdata.Free;
     end;
