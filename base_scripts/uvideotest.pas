654d720aa36dfe64f98d4d17d48b58c7de03fa53
Chris <ulrich@tcs-germany.de>
2016-08-11 14:44:28 +0200

    Videotest:advanced Video Grab Window

--- base_scripts/uvideotest.pas
+++ base_scripts/uvideotest.pas
@@ -6,20 +6,41 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  ExtCtrls, StdCtrls,uvideofunctions;
+  ExtCtrls, StdCtrls, ComCtrls,uvideofunctions, types, GraphUtil, Menus,Clipbrd;
 
 type
 
   { TfVideoTest }
 
   TfVideoTest = class(TForm)
-    eB: TEdit;
-    eR: TEdit;
-    eL: TEdit;
-    eT: TEdit;
-    WorkImage: TImage;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    PopupMenu1: TPopupMenu;
+    ScrollBar1: TScrollBar;
+    ScrollBar2: TScrollBar;
+    StatusBar1: TStatusBar;
+    ScaleTimer: TTimer;
+    PaintBox1: TPaintBox;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormMouseWheel(Sender: TObject; Shift: TShiftState;
+      WheelDelta: Integer; MousePos: TPoint; var Handled: Boolean);
+    procedure MenuItem1Click(Sender: TObject);
+    procedure MenuItem2Click(Sender: TObject);
+    procedure MenuItem3Click(Sender: TObject);
+    procedure PaintBox1MouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Integer);
+    procedure PaintBox1Paint(Sender: TObject);
+    procedure ScaleTimerTimer(Sender: TObject);
+    procedure ScrollBar1Change(Sender: TObject);
   private
     { private declarations }
+    FImage : TBitmap;
+    FScaledImage : TBitmap;
+    FScale: Double;
+    FScaled : Boolean;
+    procedure DoScalePreview;
   public
     { public declarations }
   end;
@@ -41,6 +62,21 @@ uses
   {$ENDIF}
   ;
 
+procedure TfVideoTest.DoScalePreview;
+var
+  amax: Integer;
+begin
+  FScaled := False;
+  ScaleTimer.Enabled:=True;
+  amax := abs(round(FImage.Width-1-PaintBox1.Width*FScale));
+  if aMax >0 then
+    ScrollBar1.Max:=aMax;
+  amax := abs(round(FImage.Height-1-PaintBox1.Height*FScale));
+  if aMax > 0 then
+    ScrollBar2.Max:=aMax;
+  PaintBox1.Invalidate;
+end;
+
 function CaptureImageVT(dev: PChar): Boolean;
 begin
   if not Assigned(fVideoTest) then
@@ -51,13 +87,18 @@ begin
   Application.ProcessMessages;
   Result := CaptureImage(dev);
   if Result then
-    fVideoTest.Workimage.Picture.Bitmap.LoadFromIntfImage(Image);
+    begin
+      fVideoTest.fImage.LoadFromIntfImage(Image);
+      fVideoTest.DoScalePreview;
+      fVideoTest.BringToFront;
+    end;
 end;
 
 procedure RefreshImageVT;
 begin
   RefreshImage;
-  fVideoTest.Workimage.Picture.Bitmap.LoadFromIntfImage(Image);
+  fVideoTest.fImage.LoadFromIntfImage(Image);
+  fVideoTest.DoScalePreview;
 end;
 
 function LoadImageVT(aFile: PChar): Boolean;
@@ -68,7 +109,118 @@ begin
     end;
   fVideoTest.Show;
   Result := LoadImage(aFile);
-  fVideoTest.Workimage.Picture.Bitmap.LoadFromIntfImage(Image);
+  fVideoTest.fImage.LoadFromIntfImage(Image);
+  fVideoTest.DoScalePreview;
+  fVideoTest.BringToFront;
+end;
+
+{ TfVideoTest }
+
+procedure TfVideoTest.FormCreate(Sender: TObject);
+begin
+  FScale:=1;
+  FImage := TBitmap.Create;
+  FScaledImage := TBitmap.Create;
+end;
+
+procedure TfVideoTest.FormDestroy(Sender: TObject);
+begin
+  FImage.Free;
+  FScaledImage.Free;
+end;
+
+procedure TfVideoTest.FormMouseWheel(Sender: TObject; Shift: TShiftState;
+  WheelDelta: Integer; MousePos: TPoint; var Handled: Boolean);
+begin
+  Handled := False;
+  if ssCtrl in Shift then
+    begin
+      if WheelDelta<0 then
+        FScale := FScale/1.1
+      else
+        FScale := FScale*1.1;
+      DoScalepreview;
+      Handled := True;
+    end
+  else
+    ScrollBar2.Position:=ScrollBar2.Position+15;
+  PaintBox1.Invalidate;
+end;
+
+procedure TfVideoTest.MenuItem1Click(Sender: TObject);
+begin
+  Clipboard.AsText:=StringReplace(StringReplace(StatusBar1.Panels.Items[0].Text+','+StatusBar1.Panels.Items[1].Text,'X:','',[]),'Y:','',[]);
+end;
+
+procedure TfVideoTest.MenuItem2Click(Sender: TObject);
+begin
+  Clipboard.AsText:=StringReplace(StringReplace(StringReplace(StatusBar1.Panels.Items[2].Text,' R:','',[]),' G:',',',[]),' B:',',',[]);
+end;
+
+procedure TfVideoTest.MenuItem3Click(Sender: TObject);
+begin
+  Clipboard.AsText:=StringReplace(StringReplace(StringReplace(StatusBar1.Panels.Items[3].Text,' H:','',[]),' L:',',',[]),' S:',',',[]);
+end;
+
+procedure TfVideoTest.PaintBox1MouseMove(Sender: TObject; Shift: TShiftState;
+  X, Y: Integer);
+var
+  aRGB: LongInt;
+  h: Byte;
+  l: Byte;
+  s: Byte;
+begin
+  X := X-ScrollBar1.Position;
+  Y := Y-ScrollBar2.Position;
+  StatusBar1.Panels.Items[0].Text:='X:'+IntToStr(round(X*FScale));
+  StatusBar1.Panels.Items[1].Text:='Y:'+IntToStr(round(Y*FScale));
+  aRGB := ColorToRGB(FImage.Canvas.Pixels[round(X*FScale),round(Y*FScale)]);
+  StatusBar1.Panels.Items[2].Text:=' R:'+IntToStr((aRGB) and $FF)+
+                                   ' G:'+IntToStr((aRGB shr 8) and $FF)+
+                                   ' B:'+IntToStr((aRGB shr 16) and $FF);
+  ColorToHLS(FPColorToTColor(Image.Colors[round(X*FScale),round(Y*FScale)]),h,l,s);
+  StatusBar1.Panels.Items[3].Text:=' H:'+IntToStr(h)+
+                                   ' L:'+IntToStr(l)+
+                                   ' S:'+IntToStr(s);
+end;
+
+procedure TfVideoTest.PaintBox1Paint(Sender: TObject);
+var
+  RectDest, RectSource: TRect;
+begin
+  RectDest:=Rect(0, 0, PaintBox1.Width, PaintBox1.Height);
+  if FScaled then
+    begin
+      RectSource:=Rect(
+        ScrollBar1.Position,
+        ScrollBar2.Position,
+        Scrollbar1.Position+round(PaintBox1.Width),
+        ScrollBar2.Position+round(PaintBox1.Height));
+      PaintBox1.Canvas.CopyRect(RectDest, FScaledImage.Canvas, RectSource);
+    end
+  else
+    begin
+      RectSource:=Rect(
+        ScrollBar1.Position,
+        ScrollBar2.Position,
+        Scrollbar1.Position+round(PaintBox1.Width/FScale),
+        ScrollBar2.Position+round(PaintBox1.Height/FScale));
+      PaintBox1.Canvas.CopyRect(RectDest, FImage.Canvas, RectSource);
+    end;
+end;
+
+procedure TfVideoTest.ScaleTimerTimer(Sender: TObject);
+begin
+  ScaleTimer.Enabled:=False;
+  FScaledImage.Width:=round(FImage.Width*FScale);
+  FScaledImage.Height:=round(FImage.Height*FScale);
+  FScaledImage.Canvas.StretchDraw(Rect(0,0,FScaledImage.Width,FScaledImage.Height),FImage);
+  FScaled:=True;
+end;
+
+procedure TfVideoTest.ScrollBar1Change(Sender: TObject);
+begin
+  PaintBox1.Invalidate;
 end;
 
 { TfVideoTest }
